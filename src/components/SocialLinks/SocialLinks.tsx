2da7fbcd5ce150b264936ec45b6339d39c073393
Niv Ohayon <nivohayon1582@gmail.com>
2023-10-29 17:42:14 +0200

    - Changed to a real social link

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,6 +1,7 @@
 import GitHubSVG from '../../assets/github_icon.svg';
 import InstagramSVG from '../../assets/instagram_icon.svg';
 import LinkedInSVG from '../../assets/linkedin_icon.svg';
+import { appData } from '../../data/appData';
 import './SocialLinks.css';
 
 const ICON_SIZE = 32;
@@ -8,7 +9,7 @@ const ICON_SIZE = 32;
 function SocialLinks() {
   return (
     <div className="container__SocialLinks">
-      <a href="https://github.com/bchiang7" target="_blank" rel="noreferrer">
+      <a href={appData.github_link} target="_blank" rel="noreferrer">
         <img
           width={ICON_SIZE}
           height={ICON_SIZE}
@@ -16,7 +17,7 @@ function SocialLinks() {
           alt="Github Icon"
         />
       </a>
-      <a href="https://instagram.com/bchiang7" target="_blank" rel="noreferrer">
+      <a href={appData.instagram_link} target="_blank" rel="noreferrer">
         <img
           width={ICON_SIZE}
           height={ICON_SIZE}
@@ -24,11 +25,7 @@ function SocialLinks() {
           alt="Instagram Icon"
         />
       </a>
-      <a
-        href="https://www.linkedin.com/in/bchiang7/"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href={appData.linkedin_link} target="_blank" rel="noreferrer">
         <img
           width={ICON_SIZE}
           height={ICON_SIZE}
