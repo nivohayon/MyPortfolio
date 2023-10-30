e13ab0943ab6c29c98baee8210af4e3e3dcf89df
Niv Ohayon <nivohayon1582@gmail.com>
2023-10-30 21:01:13 +0200

    Changed experience design slightly

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -40,25 +40,29 @@ function Experience() {
             <span className="from_until__Experience">
               {item.from} - {item.until}
             </span>
-            <span className="title__Experience">{item.title}</span>
-            <span className="description__Experience">{item.description}</span>
-            <div className="links_container__Experience">
-              {item.links.length > 0 &&
-                item.links.map((link) => {
-                  return (
-                    <LinkItem
-                      key={link.url}
-                      title={link.title}
-                      url={link.url}
-                    />
-                  );
-                })}
-            </div>
-            <div className="skills_container__Experience">
-              {item.skills.length > 0 &&
-                item.skills.map((skill) => {
-                  return <SkillTag key={skill} title={skill} />;
-                })}
+            <div className="data_container">
+              <span className="title__Experience">{item.title}</span>
+              <span className="description__Experience">
+                {item.description}
+              </span>
+              <div className="links_container__Experience">
+                {item.links.length > 0 &&
+                  item.links.map((link) => {
+                    return (
+                      <LinkItem
+                        key={link.url}
+                        title={link.title}
+                        url={link.url}
+                      />
+                    );
+                  })}
+              </div>
+              <div className="skills_container__Experience">
+                {item.skills.length > 0 &&
+                  item.skills.map((skill) => {
+                    return <SkillTag key={skill} title={skill} />;
+                  })}
+              </div>
             </div>
           </div>
         );
