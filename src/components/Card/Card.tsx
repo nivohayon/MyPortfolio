33d2f9ce84086918066009aa4cf2fa02a2c29dd8
Niv Ohayon <nivohayon1582@gmail.com>
2023-11-03 16:14:25 +0200

    Exported experience cards into a separate component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,64 @@
+import LinkItem from '../LinkItem/LinkItem';
+import SkillTag from '../SkillTag/SkillTag';
+import ArrowIconSVG from '../../assets/arrow_icon.svg?react';
+import './Card.css';
+
+type CardProps = {
+  experience: {
+    id: number;
+    from: string;
+    until: string;
+    title: string;
+    description: string;
+    cardClickLink: string;
+    links: {
+      title: string;
+      url: string;
+    }[];
+    skills: string[];
+  };
+};
+
+function Card({ experience }: CardProps) {
+  const handleCardClick = (link: string) => {
+    window.open(link, '_blank');
+  };
+
+  return (
+    <div
+      onClick={() => {
+        handleCardClick(experience.cardClickLink);
+      }}
+      className="container__Card"
+    >
+      <span className="from_until__Card">
+        {experience.from} - {experience.until}
+      </span>
+      <div className="data_container">
+        <div className="arrow_and_title_container__Card">
+          <span className="title__Card">{experience.title}</span>
+          <ArrowIconSVG className="arrow_icon__Card" />
+        </div>
+        <span className="description__Card">
+          {experience.description}
+        </span>
+        <div className="links_container__Card">
+          {experience.links.length > 0 &&
+            experience.links.map((link) => {
+              return (
+                <LinkItem key={link.url} title={link.title} url={link.url} />
+              );
+            })}
+        </div>
+        <div className="skills_container__Card">
+          {experience.skills.length > 0 &&
+            experience.skills.map((skill) => {
+              return <SkillTag key={skill} title={skill} />;
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,58 +1,13 @@
-import ArrowIconSVG from '../../assets/arrow_icon.svg?react';
 import { appData } from '../../data/appData';
-import LinkItem from '../LinkItem/LinkItem';
-import SkillTag from '../SkillTag/SkillTag';
+import Card from '../Card/Card';
 import './Experience.css';
 
 function Experience() {
-  const handleExperienceCardClick = (link: string) => {
-    window.open(link, '_blank');
-  };
-
   return (
     <article className="container__Experience">
       <h3 id="experience">{appData.titleExperience}</h3>
       {appData.experiences.map((item) => {
-        return (
-          <div
-            key={item.id}
-            onClick={() => {
-              handleExperienceCardClick(item.cardClickLink);
-            }}
-            className="experience_container__Experience"
-          >
-            <span className="from_until__Experience">
-              {item.from} - {item.until}
-            </span>
-            <div className="data_container">
-              <div className="arrow_and_title_container__Experience">
-                <span className="title__Experience">{item.title}</span>
-                <ArrowIconSVG className="arrow_icon__Experience" />
-              </div>
-              <span className="description__Experience">
-                {item.description}
-              </span>
-              <div className="links_container__Experience">
-                {item.links.length > 0 &&
-                  item.links.map((link) => {
-                    return (
-                      <LinkItem
-                        key={link.url}
-                        title={link.title}
-                        url={link.url}
-                      />
-                    );
-                  })}
-              </div>
-              <div className="skills_container__Experience">
-                {item.skills.length > 0 &&
-                  item.skills.map((skill) => {
-                    return <SkillTag key={skill} title={skill} />;
-                  })}
-              </div>
-            </div>
-          </div>
-        );
+        return <Card key={item.id} experience={item} />;
       })}
     </article>
   );
