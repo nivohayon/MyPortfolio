057622cdd93a082b69f1eaa4b095c90ee53628a6
Niv Ohayon <nivohayon1582@gmail.com>
2023-11-04 14:15:33 +0200

    Added theme switch

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,8 +1,9 @@
 import SunIconSVG from '../../assets/sun_icon.svg?react';
 import MoonIconSVG from '../../assets/moon_icon.svg?react';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import Store from '../../store';
 import { setTheme } from '../../store/actions/themeActions';
+import { combineClasses } from '../../utils/combineClasses';
 import './ThemeSwitch.css';
 
 function ThemeSwitch() {
@@ -10,12 +11,22 @@ function ThemeSwitch() {
     state: { theme },
     dispatchers: { dispatchTheme },
   } = useContext(Store);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
   const handleToggle = () => {
-    dispatchTheme(setTheme(theme === 'dark' ? 'light' : 'dark'));
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    dispatchTheme(setTheme(newTheme));
+    if (containerRef.current) {
+      containerRef.current.className = `container__ThemeSwitch ${newTheme}`;
+    }
   };
 
   return (
-    <div className="container__ThemeSwitch">
+    <div
+      ref={containerRef}
+      className={combineClasses('container__ThemeSwitch', theme)}
+    >
       <SunIconSVG onClick={handleToggle} className="sun_icon__ThemeSwitch" />
       <MoonIconSVG onClick={handleToggle} className="moon_icon__ThemeSwitch" />
     </div>
