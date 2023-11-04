de622777b86b9430d0af515fce251bb2bab30a35
Niv Ohayon <nivohayon1582@gmail.com>
2023-11-04 23:36:54 +0200

    Changed the style of the navbar

--- src/components/SideNavBar/SideNavBar.tsx
+++ src/components/SideNavBar/SideNavBar.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import { routes } from '../../routes/routes';
+import { combineClasses } from '../../utils/combineClasses';
 import './SideNavBar.css';
 
 const navItems = [routes.About, routes.Experience, routes.Projects];
@@ -15,11 +16,19 @@ function SideNavBar() {
             <li key={item}>
               <a className="a___SideNavBar" href={`#${item}`}>
                 <span
-                  className={`dash__SideNavBar${
-                    hash === `#${item}` ? ' selected__SideNavBar' : ''
-                  }`}
+                  className={combineClasses(
+                    'dash__SideNavBar',
+                    hash === `#${item}` ? ' selected_dash__SideNavBar' : ''
+                  )}
                 />
-                <span>{item}</span>
+                <span
+                  className={combineClasses(
+                    'a_span_text__SideNavBar',
+                    hash === `#${item}` ? ' selected_text__SideNavBar' : ''
+                  )}
+                >
+                  {item}
+                </span>
               </a>
             </li>
           );
