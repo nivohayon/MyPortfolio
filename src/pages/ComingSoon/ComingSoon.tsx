9b3d4cd79681e76205457222b49796cd6c8178c7
Niv Ohayon <nivohayon1582@gmail.com>
2023-11-04 04:03:47 +0200

    Updated doc title in coming soon screen

--- src/pages/ComingSoon/ComingSoon.tsx
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -1,7 +1,9 @@
 import { appData } from '../../data/appData';
+import useSetDocumentTitle from '../../hooks/useSetDocumentTitle';
 import './ComingSoon.css';
 
 function ComingSoon() {
+  useSetDocumentTitle(appData.textComingSoon);
   return (
     <div className="container__ComingSoon">
       <span className="text__ComingSoon">{appData.textComingSoon}</span>
